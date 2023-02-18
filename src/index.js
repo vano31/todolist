195367ab662c24601fe244fde8470b02ed4abe8c
vano31 <vanosike31@gmail.com>
2023-02-18 15:19:19 -0500

    Made itemlist and project objects

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+import {listItem, project} from './listitem.js'
+
 console.log('Izuku Dripdoria');
+
+let doHomework = listItem('Do Homework', 'Do my Homework tommorrow', 'Due on Monday', 'due at 8 AM');
+
+let writeEssay = listItem('Write Essay', 'Write Essay for Spanish class', 'Due on Tuesday', 'Due at 12 PM');
+
+let startDrawinng = listItem('Start Drawing', 'Start Drawing for Art Class', 'Due on Wednesday', 'Due at 4 PM');
+
+console.log(doHomework, writeEssay, startDrawinng);
+
+let schoolstuff = project('school');
+
+schoolstuff.addItem(doHomework);
+
+console.log(schoolstuff);
+
+let goku = 'son goku';
+
+
+
+
---
+++ src/listitem.js
@@ -0,0 +1,49 @@
+
+let listItem = function(title, description, dueDate, dueTime) {
+
+    return {title, description, dueDate, dueTime}
+
+}
+
+
+let project = function(title) {
+
+    let itemArray = [];
+
+    let addItem = function(item) {
+
+        itemArray.push(item);
+
+    }
+
+    let removeItem = function(item) {
+
+        if (item.title) {
+
+
+            //Initial version of the function that depends on simple linear search
+
+            for (let x = 0; x < itemArray.length; x++) {
+
+                if (itemArray[x].title === item.title) {
+
+                    itemArray.splice(x,1);
+                    break;
+
+                }
+
+            }
+
+            //Later version that SHOULD use binary search once the date format is integrated
+            //correctly
+
+        }
+    }
+
+    return {title, addItem, removeItem, itemArray};
+
+}
+
+
+export {listItem, project};
+

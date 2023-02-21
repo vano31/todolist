9ad8b945ee57a34463dec69beb6dbe0a4e9c5685
vano31 <vanosike31@gmail.com>
2023-02-21 11:49:15 -0500

    Fix localStorage saving of list items and projects

--- src/index.js
+++ src/index.js
@@ -1,9 +1,78 @@
 import {listItem, project} from './listitem.js'
 
-let masterArray = project('Master List')
+let masterArray = project('Master List');
+
+//Just load master array in the beginning of each page load?
+
+let newitem = document.createElement('button');
+newitem.textContent = 'Click to Create new Item';
+newitem.onclick = function() {
+
+    let newtype = prompt('What type of object is this?');
+    let newtitle =  prompt('What is the title?');
+    let newdescription = prompt('What is the description?');
+    let newduedate = prompt('What is the due date?');
+    let newduetime = prompt('What is the due time?');
+
+    let newlistItem = listItem(newtype, newtitle, newdescription, newduedate, newduetime);
+    
+
+    let oldSavedMasterArrayJSON = localStorage.getItem('masterArray');
+
+    if (oldSavedMasterArrayJSON) {
+
+        
+
+        let oldSavedMasterArray = JSON.parse(oldSavedMasterArrayJSON);
+        for (let x = 0; x < (oldSavedMasterArray.itemArray).length; x++) {
+            masterArray.addItem(oldSavedMasterArray.itemArray[x]);
+        }
+
+        masterArray.addItem(newlistItem);
+        
+        let newSavedMasterArrayJSON = JSON.stringify(masterArray);
+        localStorage.setItem('masterArray', newSavedMasterArrayJSON);
+
+    }   else {
+
+        masterArray.addItem(newlistItem);
+        let newJSON = JSON.stringify(masterArray);
+        localStorage.setItem('masterArray', newJSON);
+
+    }
+
+
+    console.log(JSON.parse(localStorage.getItem('masterArray')));
 
 
 
+}
+document.body.appendChild(newitem);
+
+
+let deleteButton = document.createElement('button');
+deleteButton.textContent = 'Delete Local Storage';
+deleteButton.onclick = function() {
+
+    localStorage.clear();
+    console.log(JSON.parse(localStorage.getItem('masterArray')));
+
+}
+document.body.appendChild(deleteButton);
+
+
+let displayStorageButton = document.createElement('button');
+displayStorageButton.textContent = 'Display Currect Storage';
+displayStorageButton.onclick = function() {
+
+    console.log(JSON.parse(localStorage.getItem('masterArray')));
+
+}
+document.body.appendChild(displayStorageButton);
+
+
+
+/*
 
 
 let doHomework = listItem('Do Homework', 'Do my Homework tommorrow', 'Due on Monday', 'due at 8 AM');
@@ -30,9 +99,9 @@ let applications = project('Work Application Steps for Different Jobs');
 work.addItem(applications);
 
 
+console.log(masterArray);
 
-
-console.log(schoolstuff);
+*/
 
 
 
